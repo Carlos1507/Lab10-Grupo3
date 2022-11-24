7b1eb2901101f686f255cb8262bd749a388b6713
Carlos1507 <ayala.carlos@pucp.edu.pe>
2022-11-23 19:49:12 -0500

    inicio

---
+++ src/test/java/com/example/lab10gtics/Lab10GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab10gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab10GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
