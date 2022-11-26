e09eb1ebacc5459876f8dd4305a8c8ef26926876
Loammi Jezreel Ugaz Manayay <a20191417@pucp.edu.pe>
2022-11-26 16:20:37 -0500

    Dos primeras vistas listas

--- src/main/resources/static/js/js1.js
+++ src/main/resources/static/js/js1.js
@@ -1,4 +1,4 @@
-
+<!--Código por Jex-->
 $(document).ready(function (){
     $('#BtnContinuar').click(function (){
         let preguntas=parseInt($('#N_RQuestions').val());
@@ -6,7 +6,9 @@ $(document).ready(function (){
         if(preguntas>3 && participantes>=3 && participantes<=5){
             $('#PrimerModal').hide();
             $('#SegundoModal').show();
-            //$('<center> <img src="/static/img/Abra.png" width="100px" height="100px"> <table> <td> <div id="color"><input type=\'text\' class="colores" /> </div> <script> $(".colores").spectrum({ color: "#fff", showPalette: true, palette: [ [\'black\', \'white\', \'blanchedalmond\'], [\'rgb(255, 128, 0);\', \'hsv 100 70 50\', \'lightyellow\'] ], change: function(color) { //alert(color.toHexString()); $(\'.container\').css("background-color",color.toHexString()); } }); </script> </td> <td> <input class="form-control" type="text" placeholder="Username"> </td> </table> <br> <div class="mb-3"> <!-- Trigger/Open The Modal --> <button onclick="document.getElementById(\'id01\').style.display=\'block\'" class="w3-button w3-green">Seleccionar personaje</button> <div id="id01" class="w3-modal"> <div class="w3-modal-content"> <div class="w3-container"> <span onclick="document.getElementById(\'id01\').style.display=\'none\'" class="w3-button w3-display-topright">&times;</span> <div class="container"> <div class="img_container" width="200px" height="200px"> <br> <img src="/static/img/Abra.png" alt="" class="main_img"> </div> <br> <div class="thumbnail_container"> <img class="thumbnail active" src="/static/img/Abra.png" alt=""> <img class="thumbnail" src="/static/img/Aerodactyl.png" alt=""> <img class="thumbnail" src="/static/img/Alakazam.png" alt=""> <img class="thumbnail" src="/static/img/Arbok.png" alt=""> <img class="thumbnail" src="/static/img/Arcanine.png" alt=""> </div> <div class="thumbnail_container"> <img class="thumbnail" src="/static/img/Articuno.png" alt=""> <img class="thumbnail" src="/static/img/Beedrill.png" alt=""> <img class="thumbnail" src="/static/img/Bellsprout.png" alt=""> <img class="thumbnail" src="/static/img/Blastoise.png" alt=""> <img class="thumbnail" src="/static/img/Bulbasaur.png" alt=""> </div> <div class="thumbnail_container"> <img class="thumbnail" src="/static/img/Butterfree.png" alt=""> <img class="thumbnail" src="/static/img/Caterpie.png" alt=""> <img class="thumbnail" src="/static/img/Chansey.png" alt=""> <img class="thumbnail" src="/static/img/Charizard.png" alt=""> <img class="thumbnail" src="/static/img/Charmander.png" alt=""> </div> <script src="/static/js/main.js"></script> </div> <script src="/static/js/main.js"></script> <br> <button class="btn btn-danger"> Seleccionar</button> <br><br> </div> </div> </div> </div> </center>').insertAfter('#navBarPlayers');
+            for (let i=0; i<(participantes-3);i++){
+                $('#display'+(i+4)).show();
+            }
         }else{
             $('#error1').remove();
             $('#error2').remove();
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,118 @@
-const main_img = document.querySelector('.main_img')
-const thumbnails = document.querySelectorAll('.thumbnail')
+const mainPup1 = document.querySelector('.mainPup1')
+const puppet1 = document.querySelectorAll('.puppet1')
 
-
-thumbnails.forEach(thumb => {
-    thumb.addEventListener('click', function(){
+puppet1.forEach(pup1 => {
+    pup1.addEventListener('click', function(){
         const active = document.querySelector('.active')
         active.classList.remove('active')
-        thumb.classList.add('active')
-        main_img.src = thumb.src
+        pup1.classList.add('active')
+        mainPup1.src = pup1.src
     })
 })
+
+const mainPup2 = document.querySelector('.mainPup2')
+const puppet2 = document.querySelectorAll('.puppet2')
+
+puppet2.forEach(pup2 => {
+    pup2.addEventListener('click', function(){
+        const active = document.querySelector('.active')
+        active.classList.remove('active')
+        pup2.classList.add('active')
+        mainPup2.src = pup2.src
+    })
+})
+
+const mainPup3 = document.querySelector('.mainPup3')
+const puppet3 = document.querySelectorAll('.puppet3')
+
+puppet3.forEach(pup2 => {
+    pup2.addEventListener('click', function(){
+        const active = document.querySelector('.active')
+        active.classList.remove('active')
+        pup2.classList.add('active')
+        mainPup3.src = pup2.src
+    })
+})
+
+const mainPup4 = document.querySelector('.mainPup4')
+const puppet4 = document.querySelectorAll('.puppet4')
+
+puppet4.forEach(pup2 => {
+    pup2.addEventListener('click', function(){
+        const active = document.querySelector('.active')
+        active.classList.remove('active')
+        pup2.classList.add('active')
+        mainPup4.src = pup2.src
+    })
+})
+
+const mainPup5 = document.querySelector('.mainPup5')
+const puppet5 = document.querySelectorAll('.puppet5')
+
+puppet5.forEach(pup2 => {
+    pup2.addEventListener('click', function(){
+        const active = document.querySelector('.active')
+        active.classList.remove('active')
+        pup2.classList.add('active')
+        mainPup5.src = pup2.src
+    })
+})
+
+
+//Coloress
+$("#colores2").spectrum({
+    color: "#fff",
+    showPalette: true,
+    palette: [
+        ['black', 'white', 'blanchedalmond'],
+        ['rgb(255, 128, 0);', 'hsv 100 70 50', 'lightyellow']
+    ],
+    change: function(color) {
+        $('#contenedor2').css("background-color",color.toHexString());
+    }
+});
+$("#colores1").spectrum({
+    color: "#fff",
+    showPalette: true,
+    palette: [
+        ['black', 'white', 'blanchedalmond'],
+        ['rgb(255, 128, 0);', 'hsv 100 70 50', 'lightyellow']
+    ],
+    change: function(color) {
+        $('#contenedor1').css("background-color",color.toHexString());
+    }
+});
+$("#colores3").spectrum({
+    color: "#fff",
+    showPalette: true,
+    palette: [
+        ['black', 'white', 'blanchedalmond'],
+        ['rgb(255, 128, 0);', 'hsv 100 70 50', 'lightyellow']
+    ],
+    change: function(color) {
+        $('#contenedor3').css("background-color",color.toHexString());
+    }
+});
+
+$("#colores4").spectrum({
+    color: "#fff",
+    showPalette: true,
+    palette: [
+        ['black', 'white', 'blanchedalmond'],
+        ['rgb(255, 128, 0);', 'hsv 100 70 50', 'lightyellow']
+    ],
+    change: function(color) {
+        $('#contenedor4').css("background-color",color.toHexString());
+    }
+});
+$("#colores5").spectrum({
+    color: "#fff",
+    showPalette: true,
+    palette: [
+        ['black', 'white', 'blanchedalmond'],
+        ['rgb(255, 128, 0);', 'hsv 100 70 50', 'lightyellow']
+    ],
+    change: function(color) {
+        $('#contenedor5').css("background-color",color.toHexString());
+    }
+});
